7ff92ef10bb5e2fc3ac59e7ea97b4f8f16b32872
Calev Eliacheff <c.eliacheff@gmail.com>
2021-09-24 10:04:10 +0200

    fix parse

--- jobs/infrastructure/NotionJobsRepository.js
+++ jobs/infrastructure/NotionJobsRepository.js
@@ -25,14 +25,40 @@ const mapToJob = (rawJob) => {
     return new Job({
         id: rawJob.id,
         title: rawJob.properties.Name.title[0].text.content,
-        mission: rawJob.properties['Mission'].rich_text[0]?.text.content || '',
-        experience: rawJob.properties['Expérience'].multi_select.map(_ => _.name),
-        location: rawJob.properties['Localisation'].multi_select.map(_ => _.name),
-        department: rawJob.properties['Ministère'].multi_select.map(_ => _.name),
-        openedToContractTypes: rawJob.properties['Poste ouvert aux'].multi_select.map(_ => _.name),
-        salary: rawJob.properties['Rémunération'].rich_text[0]?.text.content || '',
-        team: rawJob.properties['Équipe'].rich_text[0]?.text.content || ''
+        mission: parseProperty(rawJob.properties['Mission']),
+        experience: parseProperty(rawJob.properties['Expérience']),
+        location: parseProperty(rawJob.properties['Localisation']),
+        department: parseProperty(rawJob.properties['Ministère']),
+        openedToContractTypes: parseProperty(rawJob.properties['Poste ouvert aux']),
+        salary: parseProperty(rawJob.properties['Rémunération']),
+        team: parseProperty(rawJob.properties['Équipe']),
+        limitDate: parseProperty(rawJob.properties['Date limite']),
+        toCandidate: parseProperty(rawJob.properties['Pour candidater']),
+        advantage: parseProperty(rawJob.properties['Les plus du poste']),
+        contact: parseProperty(rawJob.properties['Contact']),
+        profil: parseProperty(rawJob.properties['Votre profil']),
+        conditions: parseProperty(rawJob.properties['Conditions particulières du poste']),
+        teamInfo: parseProperty(rawJob.properties['Si vous avez des questions']),
+        tasks: parseProperty(rawJob.properties['Ce que vous ferez']).split('- ').filter(item => item),
     });
+};
+
+const parseProperty = (data) => {
+    try {
+        if ('rich_text' in data) {
+            return (data.rich_text[0] || {}).plain_text || '';
+        } else if ('multi_select' in data) {
+            return data.multi_select.map(item => item.name);
+        } else if ('email' in data && data.email) {
+            return data.email[0].plain_text;
+        } else if ('date' in data && data.date) {
+            return data.date.start;
+        }
+        return '';
+    } catch (error) {
+        console.log(error);
+        return '';
+    }
 };
 
 module.exports.NotionJobsService = {
