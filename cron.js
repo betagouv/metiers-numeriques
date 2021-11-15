768143309115bb8afe57d23b9f5a8365fb94447e
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-11-15 15:06:44 +0100

    build(eslint): setup

--- cron.js
+++ cron.js
@@ -1,26 +1,29 @@
-const { CronJob } = require('cron');
-const { fetchPepJobs } = require('./src/schedulers/pepJobsScheduler');
-require('dotenv').config();
+const { CronJob } = require('cron')
+
+const { fetchPepJobs } = require('./src/schedulers/pepJobsScheduler')
+require('dotenv').config()
 
 const jobs = [
   {
-    cronTime: '0 8 * * *', // every day at 8:00
-    onTick: () => fetchPepJobs,
+    cronTime: '0 8 * * *',
     isActive: process.env.FEATURE_FLAG_FETCH_PEP_JOBS,
-    name: 'fetchPepJobs',
-  }
-];
 
-let activeJobs = 0;
+    name: 'fetchPepJobs',
+    // every day at 8:00
+    onTick: () => fetchPepJobs,
+  },
+]
+
+let activeJobs = 0
 for (const job of jobs) {
-  const cronjob = { timeZone: 'Europe/Paris', start: true, ...job };
+  const cronjob = { start: true, timeZone: 'Europe/Paris', ...job }
 
   if (cronjob.isActive) {
-    console.log(`🚀 The job "${cronjob.name}" is ON ${cronjob.cronTime}`);
-    new CronJob(cronjob);
-    activeJobs++;
+    console.log(`🚀 The job "${cronjob.name}" is ON ${cronjob.cronTime}`)
+    new CronJob(cronjob)
+    activeJobs++
   } else {
-    console.log(`❌ The job "${cronjob.name}" is OFF`);
+    console.log(`❌ The job "${cronjob.name}" is OFF`)
   }
 }
-console.log(`Started ${activeJobs} / ${jobs.length} cron jobs`);
+console.log(`Started ${activeJobs} / ${jobs.length} cron jobs`)
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-'use strict';
+require('dotenv').config()
 
-require('dotenv').config();
+const app = require('./src/server')
 
-const app = require('./src/server');
-
-const port = process.env.APP_PORT || process.env.PORT || 8080;
+const port = process.env.APP_PORT || process.env.PORT || 8080
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
+  console.log(`Server listening at http://localhost:${port}`)
+})
