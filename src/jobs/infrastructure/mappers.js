19165e652cc14e4eded96ac2161d9bea27d1c1bc
Calev Eliacheff <c.eliacheff@gmail.com>
2021-11-16 09:48:14 +0100

    typos

--- src/jobs/infrastructure/mappers.js
+++ src/jobs/infrastructure/mappers.js
@@ -25,7 +25,6 @@ const buildSlug = (title, id) => {
 }
 
 const mapToJob = (rawJob, now = Date.now()) => {
-    // console.log(rawJob.properties['Name'])
     const title = parseProperty(rawJob.properties['Name'])
     const id = rawJob.id
     const md = new MarkdownIt({linkify: true});
