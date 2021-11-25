7e0fb3f56b8544a98e469e42866d2f21aab0528c
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-11-25 07:50:54 +0100

    feat: add advantages and more to job detail

--- src/jobs/infrastructure/mappers.js
+++ src/jobs/infrastructure/mappers.js
@@ -1,16 +1,15 @@
-const MarkdownIt = require('markdown-it')
 const { startOfDay } = require('date-fns')
 const { subDays } = require('date-fns')
-const { Job, Ministry } = require('../entities')
 const { toDate } = require('date-fns-tz')
+const MarkdownIt = require('markdown-it')
 
 const parseProperty = require('../../helpers/parseProperty')
+const { Job, Ministry } = require('../entities')
 
 function urlify(text) {
-  var urlRegex = /(https?:\/\/[^\s]+)/g
-  return text.replace(urlRegex, function (url) {
-    return '<a href="' + url + '">' + url + '</a>'
-  })
+  const urlRegex = /(https?:\/\/[^\s]+)/g
+
+  return text.replace(urlRegex, url => `<a href="${url}">${url}</a>`)
   // or alternatively
   // return text.replace(urlRegex, '<a href="$1">$1</a>')
 }
@@ -24,123 +23,128 @@ const buildSlug = (title, id) => {
     .replace(/[\u0300-\u036f]/g, '')
     .replace(/ /g, '-')
     .replace(/[^\w-]+/g, '')
+
   return slug
 }
 
-const mapToJob = (rawJob, now = Date.now()) => {
-  const title = parseProperty(rawJob.properties['Name'])
-  const id = rawJob.id
+const mapToJob = rawJob => {
+  const title = parseProperty(rawJob.properties.Name)
+  const { id } = rawJob
   const md = new MarkdownIt({ linkify: true })
+  console.log(rawJob)
 
   return new Job({
-    id: rawJob.id,
-    title,
-    mission: md.renderInline(parseProperty(rawJob.properties['Mission'])),
-    experiences: parseProperty(rawJob.properties['Expérience']),
-    locations: parseProperty(rawJob.properties['Localisation']),
-    department: parseProperty(rawJob.properties['Ministère']).map(a => md.renderInline(a)),
-    entity: md.renderInline(parseProperty(rawJob.properties['Entité recruteuse']) || '') || null,
-    openedToContractTypes: parseProperty(rawJob.properties['Poste ouvert aux']),
-    salary: parseProperty(rawJob.properties['Rémunération']),
-    team: parseProperty(rawJob.properties['Équipe']),
-    limitDate: parseProperty(rawJob.properties['Date limite'])
-      ? parseProperty(rawJob.properties['Date limite'])
-      : undefined,
-    toApply: md.renderInline(parseProperty(rawJob.properties['Pour candidater'])),
-    advantages: parseProperty(rawJob.properties['Les plus du poste']),
-    contact: md.renderInline(parseProperty(rawJob.properties['Contact']) || ''),
-    profile: parseProperty(rawJob.properties['Votre profil'])
-      .split('- ')
-      .filter(item => item),
+    advantages: md.renderInline(parseProperty(rawJob.properties['Les plus du poste'])),
     conditions: parseProperty(rawJob.properties['Conditions particulières du poste'])
       .split('- ')
       .filter(item => item),
-    teamInfo: md.renderInline(parseProperty(rawJob.properties['Si vous avez des questions'])),
-    tasks: parseProperty(rawJob.properties['Ce que vous ferez'])
+    contact: md.renderInline(parseProperty(rawJob.properties.Contact) || ''),
+    department: parseProperty(rawJob.properties['Ministère']).map(a => md.renderInline(a)),
+    entity: md.renderInline(parseProperty(rawJob.properties['Entité recruteuse']) || '') || null,
+    experiences: parseProperty(rawJob.properties['Expérience']),
+    hiringProcess: md.renderInline(parseProperty(rawJob.properties['Processus de recrutement']) || '') || null,
+    id: rawJob.id,
+    limitDate: parseProperty(rawJob.properties['Date limite'])
+      ? parseProperty(rawJob.properties['Date limite'])
+      : undefined,
+    locations: parseProperty(rawJob.properties.Localisation),
+    mission: md.renderInline(parseProperty(rawJob.properties.Mission)),
+    more: md.renderInline(parseProperty(rawJob.properties['Pour en savoir plus'])),
+    openedToContractTypes: parseProperty(rawJob.properties['Poste ouvert aux']),
+    profile: parseProperty(rawJob.properties['Votre profil'])
       .split('- ')
       .filter(item => item),
-    slug: buildSlug(title, id),
-    hiringProcess: md.renderInline(parseProperty(rawJob.properties['Processus de recrutement']) || '') || null,
     publicationDate: parseProperty(rawJob.properties['Date de saisie'])
       ? parseProperty(rawJob.properties['Date de saisie'])
       : twoDaysAgo,
+    salary: parseProperty(rawJob.properties['Rémunération']),
+    slug: buildSlug(title, id),
+    tasks: parseProperty(rawJob.properties['Ce que vous ferez'])
+      .split('- ')
+      .filter(item => item),
+    team: parseProperty(rawJob.properties['Équipe']),
+    teamInfo: md.renderInline(parseProperty(rawJob.properties['Si vous avez des questions'])),
+    title,
+    toApply: md.renderInline(parseProperty(rawJob.properties['Pour candidater'])),
   })
 }
 
-const mapToMinistry = (rawMinistry, now = Date.now()) => {
-  const title = parseProperty(rawMinistry.properties['Titre'])
-  const id = rawMinistry.id
+const mapToMinistry = rawMinistry => {
+  const title = parseProperty(rawMinistry.properties.Titre)
+  const { id } = rawMinistry
   const md = new MarkdownIt()
+
   return new Ministry({
-    id,
-    title,
-    fullName: parseProperty(rawMinistry.properties['Nom complet']),
-    adress: parseProperty(rawMinistry.properties['Adresse']),
+    adress: parseProperty(rawMinistry.properties.Adresse),
     adressBis: parseProperty(rawMinistry.properties['Adresse bis']),
     brandBlock: parseProperty(rawMinistry.properties['Bloc marque']),
-    thumbnail: parseProperty(rawMinistry.properties['Vignette temporaire']),
+    challenges: md.render(parseProperty(rawMinistry.properties['Nos enjeux'])),
+    fullName: parseProperty(rawMinistry.properties['Nom complet']),
+    hiringProcess: md.render(parseProperty(rawMinistry.properties['Processus de recrutement'])),
+    id,
+    jobsLink: parseProperty(rawMinistry.properties['Toutes les offres disponibles']),
+    joinTeam: md.render(parseProperty(rawMinistry.properties['Nous rejoindre - Pourquoi?'])),
+    joinTeamMedia: parseProperty(rawMinistry.properties['Nous rejoindre - Infos']),
     keyNumbers: md.render(parseProperty(rawMinistry.properties['Les chiffres clés'])),
     keyNumbersMedia: parseProperty(rawMinistry.properties['Les chiffres clés - liens']),
     missions: md.render(parseProperty(rawMinistry.properties['Les missions'])),
-    projects: md.render(parseProperty(rawMinistry.properties['Les projets ou rélisations'])),
-    projectsMedia: parseProperty(rawMinistry.properties['Projets ou réalisations compléments']),
-    testimonials: md.render(parseProperty(rawMinistry.properties['Nos agents en parlent'])),
-    testimonialsMedia: parseProperty(rawMinistry.properties['Liens Nos agents en parlent']),
-    joinTeam: md.render(parseProperty(rawMinistry.properties['Nous rejoindre - Pourquoi?'])),
-    joinTeamMedia: parseProperty(rawMinistry.properties['Nous rejoindre - Infos']),
     motivation: md.render(parseProperty(rawMinistry.properties["Raison d'être"])),
     motivationMedia: parseProperty(rawMinistry.properties["Raison d'être complément"]),
-    profile: md.render(parseProperty(rawMinistry.properties['Ton profil'])),
-    websites: parseProperty(rawMinistry.properties['Site(s) institutionel(s)']),
-    jobsLink: parseProperty(rawMinistry.properties['Toutes les offres disponibles']),
-    values: md.render(parseProperty(rawMinistry.properties['Valeurs'])),
-    valuesMedia: parseProperty(rawMinistry.properties['Valeurs complément']),
-    schedule: md.render(parseProperty(rawMinistry.properties['Agenda'])),
-    socialNetworks: parseProperty(rawMinistry.properties['Réseaux sociaux']),
-    visualBanner: parseProperty(rawMinistry.properties['Bandeau visuel']),
-    challenges: md.render(parseProperty(rawMinistry.properties['Nos enjeux'])),
     organization: md.render(parseProperty(rawMinistry.properties['Notre organisation'])),
     organizationMedia: parseProperty(rawMinistry.properties['Notre organisation compléments']),
-    hiringProcess: md.render(parseProperty(rawMinistry.properties['Processus de recrutement'])),
+    profile: md.render(parseProperty(rawMinistry.properties['Ton profil'])),
+    projects: md.render(parseProperty(rawMinistry.properties['Les projets ou rélisations'])),
+    projectsMedia: parseProperty(rawMinistry.properties['Projets ou réalisations compléments']),
+    publicationDate: toDate('2021-09-13T00:00:00+02:00', { timeZone: 'Europe/Paris' }),
+    schedule: md.render(parseProperty(rawMinistry.properties.Agenda)),
     slug: buildSlug(title, id),
-    publicationDate: toDate('2021-09-13' + 'T00:00:00+02:00', { timeZone: 'Europe/Paris' }),
+    socialNetworks: parseProperty(rawMinistry.properties['Réseaux sociaux']),
+    testimonials: md.render(parseProperty(rawMinistry.properties['Nos agents en parlent'])),
+    testimonialsMedia: parseProperty(rawMinistry.properties['Liens Nos agents en parlent']),
+    thumbnail: parseProperty(rawMinistry.properties['Vignette temporaire']),
+    title,
+    values: md.render(parseProperty(rawMinistry.properties.Valeurs)),
+    valuesMedia: parseProperty(rawMinistry.properties['Valeurs complément']),
+    visualBanner: parseProperty(rawMinistry.properties['Bandeau visuel']),
+    websites: parseProperty(rawMinistry.properties['Site(s) institutionel(s)']),
   })
 }
 
 const formatDetailFromPep = job => {
   const item = job.properties
   const title = parseProperty(item.Name)
-  const id = job.id
+  const { id } = job
+
   return new Job({
-    id,
-    title,
-    mission: urlify(parseProperty(item.JobDescriptionTranslation_Description1_) || ''),
+    advantages: '',
+    conditions: [],
+    contact: parseProperty(item.Origin_CustomFieldsTranslation_ShortText2_),
+    department: [parseProperty(item.Origin_Entity_)],
     experiences: parseProperty(item.ApplicantCriteria_EducationLevel_)
       ? [parseProperty(item.ApplicantCriteria_EducationLevel_)]
       : [],
+    id,
+    limitDate: '',
     locations: [(parseProperty(item.Location_JobLocation_) || '').replace('- -', '')],
-    department: [parseProperty(item.Origin_Entity_)],
+    mission: urlify(parseProperty(item.JobDescriptionTranslation_Description1_) || ''),
+    more: urlify(`https://place-emploi-public.gouv.fr/offre-emploi/${parseProperty(item.Offer_Reference_)}/`),
     openedToContractTypes: parseProperty(item.JobDescription_Contract_)
       ? [parseProperty(item.JobDescription_Contract_)]
       : [],
-    salary: undefined,
-    team: '',
-    limitDate: '',
-    toApply: parseProperty(item.Origin_CustomFieldsTranslation_ShortText1_),
-    advantages: '',
-    contact: parseProperty(item.Origin_CustomFieldsTranslation_ShortText2_),
     profile: [parseProperty(item.JobDescriptionTranslation_Description2_)],
-    conditions: [],
-    teamInfo: '',
-    tasks: undefined,
-    more: urlify(`https://place-emploi-public.gouv.fr/offre-emploi/${parseProperty(item.Offer_Reference_)}/`),
     publicationDate: (parseProperty(item.FirstPublicationDate) || '').split(' ')[0],
-    slug: buildSlug(title, id) + '?tag=pep',
+    salary: undefined,
+    slug: `${buildSlug(title, id)}?tag=pep`,
+    tasks: undefined,
+    team: '',
+    teamInfo: '',
+    title,
+    toApply: parseProperty(item.Origin_CustomFieldsTranslation_ShortText1_),
   })
 }
 
 module.exports = {
-  mapToMinistry,
-  mapToJob,
   formatDetailFromPep,
+  mapToJob,
+  mapToMinistry,
 }
