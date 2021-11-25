018972129ad1e1ed8c5747332033618c0f3ff5f5
Chaïb Martinez <chaibax@gmail.com>
2021-11-25 07:32:54 +0100

    feat: add chat box crisp (#84)

---
+++ static/scripts/crisp.js
@@ -0,0 +1,9 @@
+window.$crisp = []
+window.CRISP_WEBSITE_ID = '3fbfa45d-3642-49fd-8fe8-4f11ae3539bb'
+;(function () {
+  d = document
+  s = d.createElement('script')
+  s.src = 'https://client.crisp.chat/l.js'
+  s.async = 1
+  d.getElementsByTagName('head')[0].appendChild(s)
+})()
