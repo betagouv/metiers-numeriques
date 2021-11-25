002acfb4019c880e14b95d0e7368bd44406a76b6
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2021-11-25 20:01:34 +0100

    feat: add scroll to top button (#88)

--- static/scripts/custom.js
+++ static/scripts/custom.js
@@ -1,22 +1,41 @@
 function toggleNavActive() {
-    var navs = document.querySelectorAll("#header-navigation li");
-    for (var nav of navs) {
-        var link = nav.querySelector("a")
-        var href = link.getAttribute('href')
-        var pathname = window.location.pathname
+  var navs = document.querySelectorAll('#header-navigation li')
+  for (var nav of navs) {
+    var link = nav.querySelector('a')
+    var href = link.getAttribute('href')
+    var pathname = window.location.pathname
 
-        if (pathname === '/') {
-            break
-        }
-
-        if (href.substring(1) && pathname.includes(href.substring(1))) {
-            nav.setAttribute('aria-current', 'page');
-            link.setAttribute('aria-current', 'page');
-        } else {
-            nav.removeAttribute('aria-current');
-            link.removeAttribute('aria-current');
-        }
+    if (pathname === '/') {
+      break
     }
+
+    if (href.substring(1) && pathname.includes(href.substring(1))) {
+      nav.setAttribute('aria-current', 'page')
+      link.setAttribute('aria-current', 'page')
+    } else {
+      nav.removeAttribute('aria-current')
+      link.removeAttribute('aria-current')
+    }
+  }
 }
 
-window.onload = toggleNavActive;
+window.onload = toggleNavActive
+
+const $scrollToTopButton = document.querySelector('#scroll-to-top-button')
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY < window.innerHeight) {
+    $scrollToTopButton.style.opacity = Math.round((100 * window.scrollY) / window.innerHeight) / 100
+
+    return
+  }
+
+  $scrollToTopButton.style.opacity = 1
+})
+
+$scrollToTopButton.addEventListener('click', () => {
+  window.scroll({
+    behavior: 'smooth',
+    top: 0,
+  })
+})
