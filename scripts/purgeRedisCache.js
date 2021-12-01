27485a9b109873ec448a7e21247166c9f774b2e6
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-12-01 06:27:53 +0100

    build: purge Redis cache before start

---
+++ scripts/purgeRedisCache.js
@@ -0,0 +1,25 @@
+const ß = require('bhala')
+const { createClient } = require('redis')
+
+const { REDIS_URL } = process.env
+
+const redisClient = createClient({
+  url: REDIS_URL,
+})
+
+async function purgeRedisCache() {
+  try {
+    ß.info('[scripts/purgeRedisCache.js]', 'Purging Redis cache…')
+
+    await redisClient.connect()
+    await redisClient.flushAll()
+    await redisClient.disconnect()
+
+    ß.success('[scripts/purgeRedisCache.js]', 'Redis cache purged.')
+  } catch (err) {
+    ß.error('[scripts/purgeRedisCache.js]', String(err))
+    console.error(err)
+  }
+}
+
+purgeRedisCache()
