dc93e770e07b9c6cb0245892216704af42b9948e
Chaïb Martinez <chaibax@gmail.com>
2021-12-01 18:06:58 +0100

    feat: add conversion tracking (#108)

--- static/scripts/matomo.js
+++ static/scripts/matomo.js
@@ -1,11 +1,38 @@
-var _paq = window._paq || [];
+var _paq = window._paq || []
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-  var u="https://stats.data.gouv.fr/";
-  _paq.push(['setTrackerUrl', u+'piwik.php']);
-  _paq.push(['setSiteId', '191']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-})();
+_paq.push(['trackPageView'])
+_paq.push(['enableLinkTracking'])
+;(function () {
+  var u = 'https://stats.data.gouv.fr/'
+  _paq.push(['setTrackerUrl', u + 'piwik.php'])
+  _paq.push(['setSiteId', '191'])
+  var d = document,
+    g = d.createElement('script'),
+    s = d.getElementsByTagName('script')[0]
+  g.type = 'text/javascript'
+  g.async = true
+  g.defer = true
+  g.src = u + 'piwik.js'
+  s.parentNode.insertBefore(g, s)
+})()
+
+const lireoffrebutton = document.querySelectorAll('.trk-lire-offre')
+if (lireoffrebutton) {
+  for (let i = 0; i < lireoffrebutton.length; i++) {
+    lireoffrebutton[i].addEventListener('click', function () {
+      _paq.push(['trackEvent', 'conversion', 'Click bouton lire offre'])
+    })
+  }
+}
+
+const candidature = document.querySelector('.trk-candidature')
+if (candidature) {
+  var liens = candidature.getElementsByTagName('a')
+  if (liens) {
+    for (var i = 0, len = liens.length; i < len; i++) {
+      liens[i].onclick = function () {
+        _paq.push(['trackEvent', 'conversion', 'Click lien postuler'])
+      }
+    }
+  }
+}
