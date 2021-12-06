029209c6d02c044e2a53937afa926236c105e08d
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-12-06 21:23:50 +0100

    style: improve jobs list responsiveness

--- src/helpers/stripHtmlTags.ts
+++ src/helpers/stripHtmlTags.ts
@@ -4,6 +4,10 @@ import handleError from './handleError'
 
 export default function stripHtmlTags(htmlSource: string): string {
   try {
+    if (htmlSource.length < 50) {
+      return `Description erronée 😟 ! Aidez-nous en nous le signalant par email à contact@metiers.numerique.gouv.fr.`
+    }
+
     return stripHtml(htmlSource).result
   } catch (err) {
     handleError(err, 'helpers/stripHtmlTags()')
