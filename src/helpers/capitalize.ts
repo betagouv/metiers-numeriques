30f508c922058b177b802aeccee7a9088fc5c9f6
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-01-19 05:15:42 +0100

    refactor: remove legacy code

--- src/helpers/capitalize.ts
+++
@@ -1,9 +0,0 @@
-import handleError from './handleError'
-
-export default function capitalize(text: string): string {
-  try {
-    return text.toLocaleLowerCase().replace(/(?:^|\s|["'([{])+\S/g, match => match.toUpperCase())
-  } catch (err) {
-    handleError(err, 'helpers/capitalize()')
-  }
-}
