cc3925feb3059513c87bca7cfde6fed18b97b4cf
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-01-28 09:30:03 +0100

    fix(db): clean legacy jobs email links

--- prisma/seeds/index.ts
+++ prisma/seeds/index.ts
@@ -3,6 +3,7 @@ import handleError from '../../common/helpers/handleError'
 import { initializeLegacyJobsState } from './01-initialize-legacy-jobs-state'
 import { makeLegacyJobsUpdatedAtUnique } from './02-make-legacy-jobs-updatedAt-unique'
 import { normalizeLegacyServicesRegion } from './03-normalize-legacy-services-region'
+import { cleanLegacyJobsEmailLinks } from './04-clean-legacy-jobs-email-links'
 
 async function seed() {
   const prisma = getPrisma()
@@ -11,6 +12,7 @@ async function seed() {
     await initializeLegacyJobsState(prisma)
     await makeLegacyJobsUpdatedAtUnique(prisma)
     await normalizeLegacyServicesRegion(prisma)
+    await cleanLegacyJobsEmailLinks(prisma)
   } catch (err) {
     handleError(err, 'prisma/seeds/index.ts > seed()')
   } finally {
