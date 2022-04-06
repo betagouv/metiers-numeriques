bbafa243db05954646941971adcabe06a7a7c243
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2022-04-06 16:00:07 +0200

    feat(admin): add jobs counter to dashboard (#356)

---
+++ app/molecules/AdminFigure.tsx
@@ -0,0 +1,42 @@
+import numeral from 'numeral'
+import styled from 'styled-components'
+
+import { AdminCard } from '../atoms/AdminCard'
+import { Flex } from '../atoms/Flex'
+
+const Box = styled.div`
+  margin: 0 1rem 1rem 0;
+  max-width: 20rem;
+  width: 33.33%;
+`
+
+const Label = styled.h6`
+  font-size: 100%;
+  font-weight: 500;
+`
+
+const Value = styled.p`
+  font-size: 150%;
+  font-weight: 500;
+`
+
+type AdminFigureProps = {
+  Icon: any
+  label: string
+  value?: number
+}
+export function AdminFigure({ Icon, label, value }: AdminFigureProps) {
+  return (
+    <Box>
+      <AdminCard isFirst>
+        <Flex>
+          <div>
+            <Label>{label}</Label>
+            <Value>{value !== undefined ? numeral(value).format('0,0') : '…'}</Value>
+          </div>
+          <Icon />
+        </Flex>
+      </AdminCard>
+    </Box>
+  )
+}
