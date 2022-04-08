f4965dc04b41ef52ba8c3408e4006dd664872fa7
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-04-08 10:08:16 +0200

    feat(website): add rate limiting to GraphQL public routes

---
+++ api/helpers/getRedis.ts
@@ -0,0 +1,17 @@
+import ß from 'bhala'
+import Redis from 'ioredis'
+
+const { REDIS_URL } = process.env
+if (REDIS_URL === undefined) {
+  ß.error('`REDIS_URL` environment variable is undefined.')
+  process.exit(1)
+}
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const __REDIS = {
+  redisInstance: new Redis(REDIS_URL),
+}
+
+export default function getRedis(): Redis {
+  return __REDIS.redisInstance
+}
