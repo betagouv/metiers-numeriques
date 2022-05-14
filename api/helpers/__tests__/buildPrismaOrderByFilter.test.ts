f953360cd17d8f247eb452dc9b94c502631e352c
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2022-05-14 02:53:27 +0200

    feat(admin): add new signup alerts on admin dashboard (#462)

---
+++ api/helpers/__tests__/buildPrismaOrderByFilter.test.ts
@@ -0,0 +1,28 @@
+import { buildPrismaOrderByFilter } from '../buildPrismaOrderByFilter'
+
+describe('api/helpers/buildPrismaOrderByFilter()', () => {
+  test(`with undefined`, () => {
+    const defaultOrderBy = ['aColumn', 'asc']
+
+    const result = buildPrismaOrderByFilter(defaultOrderBy as any)
+
+    expect(result).toStrictEqual({
+      orderBy: {
+        aColumn: 'asc',
+      },
+    })
+  })
+
+  test(`with ["anotherColumn", "desc"]`, () => {
+    const defaultOrderBy = ['aColumn', 'asc']
+    const orderBy = ['anotherColumn', 'desc']
+
+    const result = buildPrismaOrderByFilter(defaultOrderBy as any, orderBy as any)
+
+    expect(result).toStrictEqual({
+      orderBy: {
+        anotherColumn: 'desc',
+      },
+    })
+  })
+})
--- app/workers/alert.ts
+++ app/workers/alert.ts
@@ -1,11 +1,11 @@
 import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
-import * as R from 'ramda'
 
-import type { Recruiter } from '@prisma/client'
+import type { GetAllResponse } from '@api/resolvers/types'
+import type { Recruiter, User } from '@prisma/client'
 
-const GET_RECRUITERS = gql`
-  query GetRecruiters {
-    getRecruiters {
+const GET_LAST_INSTITUTIONLESS_RECRUITERS = gql`
+  query GetLastIntitutionLessRecruiters {
+    getRecruiters(isInstitutionless: true, pageIndex: 0, perPage: 10) {
       data {
         id
 
@@ -13,16 +13,38 @@ const GET_RECRUITERS = gql`
 
         institutionId
       }
+      length
     }
   }
 `
 
-const isRecruiterInstitutionless: (recruiter: Recruiter) => boolean = recruiter => R.isNil(recruiter.institutionId)
-const filterInstitutionlessRecruiters: (recruiter: Recruiter[]) => Recruiter[] = R.filter(isRecruiterInstitutionless)
+const GET_LAST_INACTIVE_USERS = gql`
+  query GetLastInactiveUsers {
+    getUsers(isActive: false, orderBy: ["createdAt", "desc"], pageIndex: 0, perPage: 10) {
+      data {
+        id
 
-export async function getInstitutionlessRecruiters(accessToken?: string): Promise<Recruiter[]> {
+        createdAt
+        extra {
+          requestedInstitution
+          requestedService
+        }
+        firstName
+        lastName
+      }
+      length
+    }
+  }
+`
+
+export async function getInstitutionlessRecruiters(accessToken?: string): Promise<GetAllResponse<Recruiter>> {
   if (accessToken === undefined) {
-    return []
+    return {
+      count: 0,
+      data: [],
+      index: 0,
+      length: 0,
+    }
   }
 
   const client = new ApolloClient({
@@ -34,13 +56,37 @@ export async function getInstitutionlessRecruiters(accessToken?: string): Promis
   })
 
   const {
-    data: {
-      getRecruiters: { data: recruiters },
-    },
+    data: { getRecruiters: result },
   } = await client.query({
-    query: GET_RECRUITERS,
+    query: GET_LAST_INSTITUTIONLESS_RECRUITERS,
   })
-  const institutionlessRecruiters = filterInstitutionlessRecruiters(recruiters)
 
-  return institutionlessRecruiters
+  return result
+}
+
+export async function getLastInactiveUsers(accessToken?: string): Promise<GetAllResponse<User>> {
+  if (accessToken === undefined) {
+    return {
+      count: 0,
+      data: [],
+      index: 0,
+      length: 0,
+    }
+  }
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    uri: '/api/graphql',
+  })
+
+  const {
+    data: { getUsers: result },
+  } = await client.query({
+    query: GET_LAST_INACTIVE_USERS,
+  })
+
+  return result
 }
