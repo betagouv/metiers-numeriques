a0ac59cfc7ae68a40d2763df6b7997513785037a
Pierrick Boutruche <pybuche@Pierricks-MacBook-Pro.local>
2022-06-20 11:18:53 +0200

    ref: Remove Sentry sample trace

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -8,5 +8,5 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
   dsn: SENTRY_DSN || 'https://8455fc18afa143f8a6c01c9a62d99599@o1128337.ingest.sentry.io/6170892',
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0,
 })
