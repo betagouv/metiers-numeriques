3847dbc24efd04c4e5ebba5ec78a170e33d065e4
Pierrick Boutruche <pierrick.boutruche@gmail.com>
2022-07-13 18:50:55 +0200

    feat(admin): beautify candidate pool

--- pages/admin/job/[id]/pool.tsx
+++ pages/admin/job/[id]/pool.tsx
@@ -1,13 +1,12 @@
 import { AdminTitle } from '@app/atoms/AdminTitle'
 import { Link } from '@app/atoms/Link'
-import { generateKeyFromValues } from '@app/helpers/generateKeyFromValues'
-import { AdminForm } from '@app/molecules/AdminForm'
 import { theme } from '@app/theme'
-import { JOB_CONTRACT_TYPE_LABEL, JOB_REMOTE_STATUSES_AS_OPTIONS } from '@common/constants'
+import { JOB_CONTRACT_TYPE_LABEL } from '@common/constants'
 import { handleError } from '@common/helpers/handleError'
 import { Domain, JobApplicationStatus } from '@prisma/client'
 import { Select, Button as SUIButton, Card as SUICard, Modal } from '@singularity/core'
 import { useRouter } from 'next/router'
+import * as R from 'ramda'
 import React, { useEffect, useState } from 'react'
 import { Check, GitHub, Link as LinkIcon, Linkedin, Mail, Phone, X } from 'react-feather'
 import styled from 'styled-components'
@@ -23,7 +22,7 @@ const Button = styled(SUIButton)`
 const Row = styled.div<{ centered?: boolean; fullHeight?: boolean; gap?: number }>`
   display: flex;
   flex-direction: row;
-  gap: ${p => p.gap || 2}rem;
+  gap: 1rem ${p => p.gap || 2}rem;
   ${p => (p.fullHeight ? 'height: 100%;' : '')}
   ${p => (p.centered ? 'justify-content: center;' : '')}
 `
@@ -54,6 +53,14 @@ const CandidateMenu = styled.div`
   justify-content: space-between;
   padding: 1.5rem;
   cursor: pointer;
+`
+
+const CandidateMenuInfos = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  max-width: calc(100% - 1rem);
+  flex: 1;
 `
 
 const CandidateName = styled.div`
@@ -89,7 +96,7 @@ const Spacer = styled.div<{ units?: number }>`
   margin-bottom: ${p => p.units || 1}rem;
 `
 
-const Tag = styled.div<{ color: string }>`
+const Tag = styled.div<{ color: string; onClick?: () => void }>`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -97,7 +104,9 @@ const Tag = styled.div<{ color: string }>`
   height: 1.5rem;
   border-radius: 0.75rem;
   padding: 0.25rem 0.5rem;
+  color: white;
   background-color: ${p => p.color || theme.color.neutral.greyBlue};
+  ${p => (p.onClick ? 'cursor: pointer;' : '')}
 `
 
 const ListItem = styled.div`
@@ -181,6 +190,7 @@ export default function JobApplicationPool() {
   const { id } = router.query
 
   const [showModal, setShowModal] = useState(false)
+  const [statusFilter, setStatusFilter] = useState<JobApplicationStatus>()
   const [applications, setApplications] = useState<JobApplicationWithRelation[]>([])
   const [currentApplication, setCurrentApplication] = useState<JobApplicationWithRelation>()
   const [isLoading, setIsLoading] = useState(false)
@@ -253,28 +263,63 @@ export default function JobApplicationPool() {
   }
 
   const currentCandidate = currentApplication?.candidate
+  const applicationStatusCounts = R.countBy(application => application.status, applications)
 
   return (
     <>
+      <AdminTitle>Candidature: {id}</AdminTitle>
+      <Spacer units={1} />
       <Row fullHeight>
         <Col scroll size={20}>
           <CandidateList>
-            {applications.map(application => (
-              <CandidateMenu onClick={() => setCurrentApplication(application)}>
-                <div>
-                  <CandidateName>{getCandidateFullName(application.candidate)}</CandidateName>
-                  <CandidateInfo>{application.candidate.currentJob}</CandidateInfo>
-                  <CandidateInfo>{formatSeniority(application.candidate.yearsOfExperience)}</CandidateInfo>
-                  {application.status === JobApplicationStatus.ACCEPTED && (
-                    <Tag color={theme.color.success.mint}>Dans mon vivier</Tag>
-                  )}
-                  {application.status === JobApplicationStatus.REJECTED && (
-                    <Tag color={theme.color.danger.rubicund}>Refusé</Tag>
-                  )}
-                </div>
-                {application.candidate.id === currentCandidate?.id && <Dot />}
-              </CandidateMenu>
-            ))}
+            <Row gap={1} style={{ padding: '1.5rem' }}>
+              <span>Filtres:</span>
+              <Tag
+                color={
+                  statusFilter === JobApplicationStatus.ACCEPTED ? theme.color.success.herbal : theme.color.success.mint
+                }
+                onClick={() =>
+                  setStatusFilter(currentFilter =>
+                    currentFilter === JobApplicationStatus.ACCEPTED ? undefined : JobApplicationStatus.ACCEPTED,
+                  )
+                }
+              >
+                Vivier ({applicationStatusCounts[JobApplicationStatus.ACCEPTED] || 0})
+              </Tag>
+              <Tag
+                color={
+                  statusFilter === JobApplicationStatus.REJECTED
+                    ? theme.color.danger.raspberry
+                    : theme.color.danger.rubicund
+                }
+                onClick={() =>
+                  setStatusFilter(currentFilter =>
+                    currentFilter === JobApplicationStatus.REJECTED ? undefined : JobApplicationStatus.REJECTED,
+                  )
+                }
+              >
+                Refusés ({applicationStatusCounts[JobApplicationStatus.REJECTED] || 0})
+              </Tag>
+            </Row>
+            {applications
+              .filter(application => (statusFilter ? application.status === statusFilter : true))
+              .map(application => (
+                <CandidateMenu onClick={() => setCurrentApplication(application)}>
+                  <CandidateMenuInfos>
+                    <CandidateName>{getCandidateFullName(application.candidate)}</CandidateName>
+                    <CandidateInfo>{application.candidate.currentJob}</CandidateInfo>
+                    <CandidateInfo>{formatSeniority(application.candidate.yearsOfExperience)}</CandidateInfo>
+                    <Spacer units={0.5} />
+                    {application.status === JobApplicationStatus.ACCEPTED && (
+                      <Tag color={theme.color.success.mint}>Dans mon vivier</Tag>
+                    )}
+                    {application.status === JobApplicationStatus.REJECTED && (
+                      <Tag color={theme.color.danger.rubicund}>Refusé</Tag>
+                    )}
+                  </CandidateMenuInfos>
+                  {application.candidate.id === currentCandidate?.id && <Dot />}
+                </CandidateMenu>
+              ))}
           </CandidateList>
         </Col>
         <Col size={80}>
@@ -289,7 +334,7 @@ export default function JobApplicationPool() {
                       {currentCandidate.currentJob} • {formatSeniority(currentCandidate.yearsOfExperience)}
                     </ApplicationSubtitle>
                     <Spacer units={1} />
-                    <Row>
+                    <Row style={{ flexWrap: 'wrap' }}>
                       <Row gap={0.5}>
                         <Mail />
                         <Link href={`mailto:${currentCandidate.user.email}`} rel="noreferrer" target="_blank">
@@ -330,11 +375,25 @@ export default function JobApplicationPool() {
                     <Spacer units={3} />
 
                     <Row centered>
-                      <Button accent="danger" onClick={() => setShowModal(true)}>
-                        <X /> Refuser cette candidature
+                      <Button
+                        accent="danger"
+                        disabled={currentApplication.status === JobApplicationStatus.REJECTED}
+                        onClick={() => setShowModal(true)}
+                      >
+                        <X />{' '}
+                        {currentApplication.status === JobApplicationStatus.REJECTED
+                          ? 'Candidature refusée'
+                          : 'Refuser cette candidature'}
                       </Button>
-                      <Button accent="success" onClick={() => handleAccepted(currentApplication.id)}>
-                        <Check /> Mettre dans mon vivier
+                      <Button
+                        accent="success"
+                        disabled={currentApplication.status === JobApplicationStatus.ACCEPTED}
+                        onClick={() => handleAccepted(currentApplication.id)}
+                      >
+                        <Check />{' '}
+                        {currentApplication.status === JobApplicationStatus.ACCEPTED
+                          ? 'Présent dans le vivier'
+                          : 'Mettre dans mon vivier'}
                       </Button>
                     </Row>
                     <Spacer units={3} />
